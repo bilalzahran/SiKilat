dfff380ce16ef29d7f92bde8da22d4097528eb03
bilalzahran <bilalzahran08@gmail.com>
2019-01-25 07:14:15 +0700

    menambahkan file 'dasar' database.sql

---
+++ database.sql
@@ -0,0 +1,12 @@
+-- Ini adalah file untuk membuat database MySQL
+-- Setiap orang punya kontribusi masing-masin di querynya
+-- Dari mulai membuat table dan membuat query SELECT dll
+
+
+-- Bilal.
+
+-- Afri.
+
+-- Astrid.
+
+-- Yulinda.
