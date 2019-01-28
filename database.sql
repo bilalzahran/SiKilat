ead2c1f8b96946e8b8807b39ca87e74369dd8c0a
astridvelia <veliaastrid98@gmail.com>
2019-01-28 15:51:25 +0700

    Menambahkan query create table pengirim

--- database.sql
+++ database.sql
@@ -8,5 +8,16 @@
 -- Afri.
 
 -- Astrid.
+CREATE TABLE `sikilat_db`.`pengirim` ( 
+	`id` INT(10) NOT NULL AUTO_INCREMENT , 
+	`nama_pengirim` VARCHAR(30) NOT NULL , 
+	`alamat` VARCHAR(50) NOT NULL , 
+	`no_telp` VARCHAR(13) NOT NULL , 
+	`username` VARCHAR(30) NOT NULL , 
+	`password` VARCHAR(30) NOT NULL , 
+	`foto` VARCHAR(100) NOT NULL , 
+	PRIMARY KEY (`id`(10)));
+
+ 
 
 -- Yulinda.
