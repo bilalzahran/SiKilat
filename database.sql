c091d58036bf3780446f13923c507a93eb87ced4
afrimaiyanto <afrimaiyanto001@gmail.com>
2019-01-28 15:02:02 +0700

    Menambahkan query create table barang

--- database.sql
+++ database.sql
@@ -7,6 +7,13 @@
 
 -- Afri.
 
+-- Membuat tabel
+CREATE TABLE `sikilat_db`.`barang` ( 
+ `id` INT(10) NOT NULL AUTO_INCREMENT ,
+ `nama_barang` VARCHAR(30) NOT NULL ,
+ `berat` FLOAT(3) NOT NULL ,
+ `deskripsi` VARCHAR(100) NOT NULL ,
+ PRIMARY KEY (`id`));
 -- Astrid.
 
 -- Yulinda.
