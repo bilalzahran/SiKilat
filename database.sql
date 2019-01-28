b7e9b6e7b6717e35a50ce0cf3865060c0589fa5d
LindaLubis <46948501+LindaLubis@users.noreply.github.com>
2019-01-28 14:24:52 +0700

    Menambahkan query create table admin

--- database.sql
+++ database.sql
@@ -10,3 +10,13 @@
 -- Astrid.
 
 -- Yulinda.
+CREATE TABLE `sikilat_db`.`admin` ( 
+	`id` INT(10) NOT NULL AUTO_INCREMENT , 
+	`username` VARCHAR(30) NOT NULL , 
+	`password` VARCHAR(30) NOT NULL , 
+	`nama` VARCHAR(30) NOT NULL , 
+	`alamat` VARCHAR(30) NOT NULL , 
+	`no_telp` VARCHAR(13) NOT NULL , 
+	`foto` VARCHAR(100) NOT NULL , 
+	`level` INT(1) NOT NULL , 
+	PRIMARY KEY (`id`)) ;
