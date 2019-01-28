f5dc54bf93160c7dcc50cf8a4372139567017dbe
LindaLubis <46948501+LindaLubis@users.noreply.github.com>
2019-01-28 16:24:25 +0700

    menambahkan query insert table

--- database.sql
+++ database.sql
@@ -25,3 +25,27 @@ CREATE TABLE `sikilat_db`.`pembayaran` ( `id` INT(10) NOT NULL AUTO_INCREMENT,
 	`jumlah` INT(7) NOT NULL ,
 	 `tanggal` DATETIME NOT NULL , 
 	`id_laporan` INT(10) NOT NULL );
+
+INSERT INTO `admin` (`id`, `username`, `password`, `nama`, `alamat`, `no_telp`, `foto`, `level`)
+VALUES (NULL, 'ani', '09april', 'ani', 'padang', '0822837804367', 'http://foto.com', '1');
+
+INSERT INTO `admin` (`id`, `username`, `password`, `nama`, `alamat`, `no_telp`, `foto`, `level`) 
+VALUES (NULL, 'ayu', '23sepetember', 'ayu', 'medan', '082266789326', 'http://foto.com', '2'),
+	(NULL, 'vika', '02november', 'vika', 'pekanbaru', '0822780091122', 'http://foto.com', '1');
+
+ INSERT INTO `admin` (`id`, `username`, `password`, `nama`, `alamat`, `no_telp`, `foto`, `level`)
+ VALUES (NULL, 'iyang', '26desember', 'iyang', 'lampung', '0822837804311', 'http://foto.com', '2'), 
+ 	(NULL, 'linda', '11oktober', 'linda', 'bandung', '0822780091126', 'http://foto.com', '2');
+
+INSERT INTO `pembayaran` (`id`, `jumlah`, `tanggal`, `id_laporan`)
+ VALUES (NULL, '10000', '2019-01-01 00:00:00', '1'),
+  	(NULL, '20000', '2019-01-02 00:20:20', '2');
+
+INSERT INTO `pembayaran` (`id`, `jumlah`, `tanggal`, `id_laporan`) 
+VALUES (NULL, '30000', '2019-01-02 00:00:50', '3'),
+ (NULL, '40000', '2019-01-04 00:10:10', '4');
+
+ INSERT INTO `pembayaran` (`id`, `jumlah`, `tanggal`, `id_laporan`) 
+ VALUES (NULL, '50000', '2019-01-05 00:05:20', '5');
+
+ 
