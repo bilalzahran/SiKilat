714e3a95b848ee461bf61d774343e28465202ee4
bilalzahran <bilalzahran08@gmail.com>
2019-01-29 17:28:01 +0700

    Revisi query create table, insert data laporan_pembayaran dan table barang.

--- database.sql
+++ database.sql
@@ -13,28 +13,31 @@ CREATE TABLE barang (
  nama_barang VARCHAR(30) NOT NULL ,
  berat FLOAT(3) NOT NULL ,
  deskripsi VARCHAR(100) NOT NULL ,
- PRIMARY KEY (id)
+ id_pengiriman INT(10),
+ PRIMARY KEY (id),
+ FOREIGN KEY (id_pengiriman) REFERENCES pengiriman(id_pengiriman)
  );
  
  CREATE TABLE laporan_pembayaran ( 
  id_laporan INT(10) NOT NULL AUTO_INCREMENT , 
  tanggal DATE NOT NULL , 
  id_admin INT(10) NOT NULL , 
- PRIMARY KEY (id_laporan)
+ PRIMARY KEY (id_laporan),
+ FOREIGN KEY (id_admin) REFERENCES admin(id_admin)
  );
  
  --memasukan record/data
- INSERT INTO barang VALUES (NULL, 'kipas', 2, 'kipas elektronik');
- INSERT INTO barang VALUES (NULL, 'tas', 1, 'tas ransel');
- INSERT INTO barang VALUES (NULL, 'drone', 2, 'drone mini phantom');
- INSERT INTO barang VALUES (NULL, 'laptop', 1, 'laptop asus');
- INSERT INTO barang VALUES (NULL, 'televisi', 3, 'tv toshiba 21 inc');
+ INSERT INTO barang VALUES (NULL, 'kipas', 2, 'kipas elektronik',1);
+ INSERT INTO barang VALUES (NULL, 'tas', 1, 'tas ransel',2);
+ INSERT INTO barang VALUES (NULL, 'drone', 2, 'drone mini phantom',3);
+ INSERT INTO barang VALUES (NULL, 'laptop', 1, 'laptop asus',4);
+ INSERT INTO barang VALUES (NULL, 'televisi', 3, 'tv toshiba 21 inc',5);
  
- INSERT INTO laporan_pembayaran VALUES (NULL, '2018-01-01', 1301160794);
- INSERT INTO laporan_pembayaran VALUES (NULL, '2018-02-01', 1301160784);
- INSERT INTO laporan_pembayaran VALUES (NULL, '2018-03-01', 1301160784);
- INSERT INTO laporan_pembayaran VALUES (NULL, '2018-04-01', 1301160730);
- INSERT INTO laporan_pembayaran VALUES (NULL, '2018-05-01', 1301160779);
+ INSERT INTO laporan_pembayaran VALUES (NULL, '2018-01-01', 1);
+ INSERT INTO laporan_pembayaran VALUES (NULL, '2018-02-01',	2);
+ INSERT INTO laporan_pembayaran VALUES (NULL, '2018-03-01', 3);
+ INSERT INTO laporan_pembayaran VALUES (NULL, '2018-04-01', 4);
+ INSERT INTO laporan_pembayaran VALUES (NULL, '2018-05-01', 5);
  
  --view record/data
  SELECT * FROM barang;
