e07ab148b59442d34a711df221d6a243cb160856
bilalzahran <bilalzahran08@gmail.com>
2019-01-29 17:24:19 +0700

    Revisi query create table pengiriman dan insert data pengiriman

--- database.sql
+++ database.sql
@@ -17,19 +17,22 @@ CREATE TABLE pengiriman(
 	provinsi VARCHAR(30),
 	kodepos VARCHAR(6),
 	jenis_pengiriman VARCHAR(30),
+	tanggal_pengiriman DATETIME,
 	status_pengiriman VARCHAR(30),
+	id_laporan INT(10),
 	PRIMARY KEY(id),
-	FOREIGN KEY(id_pengirim) REFERENCES pengirim(id)
+	FOREIGN KEY(id_pengirim) REFERENCES pengirim(id),
+	FOREIGN KEY(id_laporan) REFERENCES laporan_pengiriman(id)
 );
 -- Insert Data
 INSERT INTO pengiriman 
-(id,no_resi,id_pengirim,nama_penerima,alamat_penerima,no_telp_penerima,kecamatan,kota,provinsi,kodepos,jenis_pengiriman,status)
+(id,no_resi,id_pengirim,nama_penerima,alamat_penerima,no_telp_penerima,kecamatan,kota,provinsi,kodepos,jenis_pengiriman,status,id_laporan)
 VALUES
-('CGK123412','1','Agung','Jalan pondok lima no.3','082314231234','Bojongsoang','Bandung','Jawa Barat','40287','Reguler','On Process'),
-('CGK123413','2','Mamat','Jalan pondok empat no.4','082314231234','Buahbatu','Bandung','Jawa Barat','40223','Reguler','On Process'),
-('CGK123414','3','Andri','Jalan pondok tiga no.5','082314231234','Kopo','Bandung','Jawa Barat','40211','Reguler','On Process'),
-('CGK123415','4','Yulia','Jalan pondok dua no.6','082314231234','Marga Asih','Bandung','Jawa Barat','40223','Reguler','On Process'),
-('CGK123416','5','Siska','Jalan pondok satu no.7','082314231234','Kiaracondong','Bandung','Jawa Barat','40232','Reguler','On Process');
+('CGK123412','1','Agung','Jalan pondok lima no.3','082314231234','Bojongsoang','Bandung','Jawa Barat','40287','Reguler','2019-01-07 00:00:00','On Process','1'),
+('CGK123413','2','Mamat','Jalan pondok empat no.4','082314231234','Buahbatu','Bandung','Jawa Barat','40223','Reguler','2019-01-07 00:00:00','On Process','2'),
+('CGK123414','3','Andri','Jalan pondok tiga no.5','082314231234','Kopo','Bandung','Jawa Barat','40211','Reguler','2019-01-07 00:00:00','On Process','3'),
+('CGK123415','4','Yulia','Jalan pondok dua no.6','082314231234','Marga Asih','Bandung','Jawa Barat','40223','Reguler','2019-01-07 00:00:00','On Process','4'),
+('CGK123416','5','Siska','Jalan pondok satu no.7','082314231234','Kiaracondong','Bandung','Jawa Barat','40232','Reguler','2019-01-07 00:00:00','On Process','5');
 -- Select Data
 SELECT * FROM pengiriman;
 
