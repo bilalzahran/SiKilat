f603667a12f353a0e2f87cd287f811bd7760c731
bilalzahran <bilalzahran08@gmail.com>
2019-01-29 17:21:07 +0700

    Revisi query create table pengirim dan laporan_pengiriman

--- database.sql
+++ database.sql
@@ -8,21 +8,22 @@
 -- Afri.
 
 -- Astrid.
-CREATE TABLE `sikilat_db`.`pengirim` ( 
-	`id` INT(10) NOT NULL AUTO_INCREMENT , 
-	`nama_pengirim` VARCHAR(30) NOT NULL , 
-	`alamat` VARCHAR(50) NOT NULL , 
-	`no_telp` VARCHAR(13) NOT NULL , 
-	`username` VARCHAR(30) NOT NULL , 
-	`password` VARCHAR(30) NOT NULL , 
-	`foto` VARCHAR(100) NOT NULL , 
-	PRIMARY KEY (`id`(10)));
+CREATE TABLE pengirim ( 
+	id INT(10) NOT NULL AUTO_INCREMENT , 
+	nama_pengirim VARCHAR(30) NOT NULL , 
+	alamat VARCHAR(50) NOT NULL , 
+	no_telp VARCHAR(13) NOT NULL , 
+	username VARCHAR(30) NOT NULL , 
+	password VARCHAR(30) NOT NULL , 
+	foto VARCHAR(100) NOT NULL , 
+	PRIMARY KEY (id));
 
- CREATE TABLE `sikilat_db`.`laporan_pengiriman` ( 
- 	`id` INT(10) NOT NULL AUTO_INCREMENT ,
- 	`tanggal` DATETIME NOT NULL , 
- 	`id_admin` INT(10) NOT NULL , 
- 	PRIMARY KEY (`id`)); 
+ CREATE TABLE laporan_pengiriman ( 
+ 	id INT(10) NOT NULL AUTO_INCREMENT ,
+ 	tanggal DATETIME NOT NULL , 
+ 	id_admin INT(10) NOT NULL , 
+ 	PRIMARY KEY (id),
+ 	FOREIGN KEY (id_admin) REFERENCES admin(id)); 
 
  INSERT INTO `pengirim` (`id`, `nama_pengirim`, `alamat`, `no_telp`, `username`, `password`, `foto`) VALUES 
  (NULL, 'Bilal', 'GBA 2', '085956243648', 'bilal', 'bial', 'linkfoto'),
