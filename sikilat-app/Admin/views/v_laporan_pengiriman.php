300e8771dd8447d8f01e0baa472c19bb9385cba6
husnul khotimahh <husnulkhotimahh540@gmail.com>
2019-02-05 16:04:50 +0700

    menambahkan tampilan laporan pembayaran dan pengiriman

---
+++ sikilat-app/Admin/views/v_laporan_pengiriman.php
@@ -0,0 +1,60 @@
+                        <ul class="list-inline menu-left mb-0">
+                            <li class="float-left">
+                                <button class="button-menu-mobile open-left disable-btn">
+                                    <i class="dripicons-menu"></i>
+                                </button>
+                            </li>
+                            <li>
+                                <div class="page-title-box">
+                                    <h4 class="page-title"> Laporan Pengiriman </h4>
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item"><a href="#">Laporan</a></li>
+                                        <li class="breadcrumb-item active">Pengiriman</li>
+                                    </ol>
+                                </div>
+                            </li>
+                        </ul>
+                </nav>
+
+            </div>
+            <!-- Top Bar End -->
+
+
+
+            <!-- Start Page content -->
+            <div class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="card-box">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <h3 class="d-inline title">Tabel Laporan Pengiriman</h3>        
+                                    </div>
+                                    <div class="col-md-6">
+                                        
+                                         <button type="button" class="btn btn-primary waves-light waves-effect pull-right d-inline"> Tambah Laporan </button>  
+                                    </div>
+                                </div>
+                                <br>                       
+                                <table class="table table-striped mb-0">
+                                    <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Tanggal</th>
+                                        <th>Admin</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td><button type="button" class="btn btn-light waves-effect">Detail</button></td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
